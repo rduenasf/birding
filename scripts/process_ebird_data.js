81ae9cc79df976e2fb7017d29ca37002e612b1b9
Rodrigo Dueñas <rduenasf@gmail.com>
2024-12-21 16:10:40 -0800

    refactor processing script

--- scripts/process_ebird_data.js
+++ scripts/process_ebird_data.js
@@ -28,145 +28,164 @@ const toCamelCase = (str) => {
     .replace(/\s+/g, "");
 };
 
-// Read the ML_USER4436073.csv file and create a mapping from ML Catalog Number to metadata
-fs.createReadStream(mlFile)
-  .pipe(csv())
-  .on("data", (row) => {
+// Function to read CSV file and return a promise
+const readCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => resolve(results))
+      .on("error", (error) => reject(error));
+  });
+};
+
+// Function to process ML data
+const processMLData = async () => {
+  const mlData = await readCSV(mlFile);
+  mlData.forEach((row) => {
     const camelCaseRow = {};
     for (const key in row) {
       camelCaseRow[toCamelCase(key)] = row[key];
     }
     const mlCatalogNumber = camelCaseRow["MlCatalogNumber"];
     mlDataMap[mlCatalogNumber] = camelCaseRow;
-  })
-  .on("end", () => {
-    // Read the taxonomy file and create a mapping from scientific name to species code
-    fs.createReadStream(taxonomyFile)
-      .pipe(csv())
-      .on("data", (row) => {
-        const speciesCode = row["SPECIES_CODE"];
-        speciesCodeMap[row["SCI_NAME"]] = speciesCode;
-        dataDict[speciesCode] = {
-          taxonOrder: row["TAXON_ORDER"],
-          category: row["CATEGORY"],
-          speciesCode: row["SPECIES_CODE"],
-          taxonConceptId: row["TAXON_CONCEPT_ID"],
-          primaryComName: row["PRIMARY_COM_NAME"],
-          sciName: row["SCI_NAME"],
-          order: row["ORDER"],
-          family: row["FAMILY"],
-          speciesGroup: row["SPECIES_GROUP"],
-          reportAs: row["REPORT_AS"],
-          observations: [],
-        };
-      })
-      .on("end", () => {
-        // Read the eBird data file and process it
-        fs.createReadStream(ebirdInputFile)
-          .pipe(csv())
-          .on("data", (row) => {
-            const scientificName = row["Scientific Name"];
-            const speciesCode = speciesCodeMap[scientificName];
-            if (!speciesCode) {
-              console.warn(
-                `No species code found for scientific name: ${scientificName}`
-              );
-              return;
-            }
-            // Combine Date and Time into a single DateTime field
-            row["DateTime"] = new Date(`${row["Date"]} ${row["Time"]}`);
-
-            // Remove Date and Time fields
-            delete row["Date"];
-            delete row["Time"];
-
-            // Convert keys to camelCase and remove text within parentheses
-            const camelCaseRow = {};
-            for (const key in row) {
-              camelCaseRow[toCamelCase(key)] = row[key];
-            }
-
-            dataDict[speciesCode].observations.push(camelCaseRow);
-
-            // Update state species count
-            const country = camelCaseRow["state/Province"].split("-")[0];
-            if (!countrySpeciesCount[country]) {
-              countrySpeciesCount[country] = new Set();
-            }
-            countrySpeciesCount[country].add(speciesCode);
-
-            // Update country species count
-            if (!speciesCountryCount[speciesCode]) {
-              speciesCountryCount[speciesCode] = {};
-            }
-            if (!speciesCountryCount[speciesCode][country]) {
-              speciesCountryCount[speciesCode][country] = new Set();
-            }
-            speciesCountryCount[speciesCode][country].add(
-              camelCaseRow["locationId"]
-            );
-          })
-          .on("end", () => {
-            // Remove entries without observations
-            for (const key in dataDict) {
-              if (dataDict[key].observations.length === 0) {
-                delete dataDict[key];
-              }
-            }
-
-            // Replace mlCatalogNumbers with metadata objects
-            for (const key in dataDict) {
-              dataDict[key].observations.forEach((observation) => {
-                if (observation.mlCatalogNumbers) {
-                  observation.mlCatalogNumbers = observation.mlCatalogNumbers
-                    .split(" ")
-                    .map((id) => mlDataMap[id] || id);
-                }
-              });
-            }
-
-            // Sort each array by the new DateTime field
-            for (const key in dataDict) {
-              dataDict[key].observations.sort((a, b) => {
-                return new Date(a["dateTime"]) - new Date(b["dateTime"]);
-              });
-            }
-
-            const jsonData = JSON.stringify(dataDict, null, 4);
-            fs.writeFileSync(ebirdOutputFile, jsonData, "utf-8");
-            console.log(`JSON data has been written to ${ebirdOutputFile}`);
-
-            // Create summary data
-            const countrySummary = {};
-            for (const country in countrySpeciesCount) {
-              countrySummary[country] = countrySpeciesCount[country].size;
-            }
-
-            fs.writeFileSync(
-              countrySummaryOutputFile,
-              JSON.stringify(countrySummary, null, 2)
-            );
-            console.log(
-              `Summary JSON file has been created at ${countrySummaryOutputFile}`
-            );
-
-            const countrySpeciesSummary = {};
-            for (const species in speciesCountryCount) {
-              countrySpeciesSummary[species] = {};
-              for (const country in speciesCountryCount[species]) {
-                countrySpeciesSummary[species][country] =
-                  speciesCountryCount[species][country].size;
-              }
-            }
-
-            // Write country species count data
-            fs.writeFileSync(
-              countrySpeciesOutputFile,
-              JSON.stringify(countrySpeciesSummary, null, 2)
-            );
-            console.log(
-              `Country species count JSON file has been created at ${countrySpeciesOutputFile}`
-            );
-          });
-      });
   });
+};
+
+// Function to process taxonomy data
+const processTaxonomyData = async () => {
+  const taxonomyData = await readCSV(taxonomyFile);
+  taxonomyData.forEach((row) => {
+    const speciesCode = row["SPECIES_CODE"];
+    speciesCodeMap[row["SCI_NAME"]] = speciesCode;
+    dataDict[speciesCode] = {
+      taxonOrder: row["TAXON_ORDER"],
+      category: row["CATEGORY"],
+      speciesCode: row["SPECIES_CODE"],
+      taxonConceptId: row["TAXON_CONCEPT_ID"],
+      primaryComName: row["PRIMARY_COM_NAME"],
+      sciName: row["SCI_NAME"],
+      order: row["ORDER"],
+      family: row["FAMILY"],
+      speciesGroup: row["SPECIES_GROUP"],
+      reportAs: row["REPORT_AS"],
+      observations: [],
+    };
+  });
+};
+
+// Function to process eBird data
+const processEBirdData = async () => {
+  const ebirdData = await readCSV(ebirdInputFile);
+  ebirdData.forEach((row) => {
+    const scientificName = row["Scientific Name"];
+    const speciesCode = speciesCodeMap[scientificName];
+    if (!speciesCode) {
+      console.warn(
+        `No species code found for scientific name: ${scientificName}`
+      );
+      return;
+    }
+    // Combine Date and Time into a single DateTime field
+    row["DateTime"] = new Date(`${row["Date"]} ${row["Time"]}`);
+
+    // Remove Date and Time fields
+    delete row["Date"];
+    delete row["Time"];
+
+    // Convert keys to camelCase and remove text within parentheses
+    const camelCaseRow = {};
+    for (const key in row) {
+      camelCaseRow[toCamelCase(key)] = row[key];
+    }
+
+    dataDict[speciesCode].observations.push(camelCaseRow);
+
+    // Update state species count
+    const country = camelCaseRow["state/Province"].split("-")[0];
+    if (!countrySpeciesCount[country]) {
+      countrySpeciesCount[country] = new Set();
+    }
+    countrySpeciesCount[country].add(speciesCode);
+
+    // Update country species count
+    if (!speciesCountryCount[speciesCode]) {
+      speciesCountryCount[speciesCode] = {};
+    }
+    if (!speciesCountryCount[speciesCode][country]) {
+      speciesCountryCount[speciesCode][country] = new Set();
+    }
+    speciesCountryCount[speciesCode][country].add(camelCaseRow["locationId"]);
+  });
+};
+
+// Function to remove entries without observations
+const removeEmptyEntries = () => {
+  for (const key in dataDict) {
+    if (dataDict[key].observations.length === 0) {
+      delete dataDict[key];
+    }
+  }
+};
+
+// Function to replace mlCatalogNumbers with metadata objects
+const replaceMlCatalogNumbers = () => {
+  for (const key in dataDict) {
+    dataDict[key].observations.forEach((observation) => {
+      if (observation.mlCatalogNumbers) {
+        observation.mlCatalogNumbers = observation.mlCatalogNumbers
+          .split(" ")
+          .map((id) => mlDataMap[id] || id);
+      }
+    });
+  }
+};
+
+// Function to sort observations by DateTime
+const sortObservations = () => {
+  for (const key in dataDict) {
+    dataDict[key].observations.sort((a, b) => {
+      return new Date(a["dateTime"]) - new Date(b["dateTime"]);
+    });
+  }
+};
+
+// Function to write JSON data to file
+const writeJSONToFile = (filePath, data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
+  console.log(`JSON data has been written to ${filePath}`);
+};
+
+// Function to create summary data
+const createSummaryData = () => {
+  const countrySummary = {};
+  for (const country in countrySpeciesCount) {
+    countrySummary[country] = countrySpeciesCount[country].size;
+  }
+  writeJSONToFile(countrySummaryOutputFile, countrySummary);
+
+  const countrySpeciesSummary = {};
+  for (const species in speciesCountryCount) {
+    countrySpeciesSummary[species] = {};
+    for (const country in speciesCountryCount[species]) {
+      countrySpeciesSummary[species][country] =
+        speciesCountryCount[species][country].size;
+    }
+  }
+  writeJSONToFile(countrySpeciesOutputFile, countrySpeciesSummary);
+};
+
+// Main function to process all data
+const main = async () => {
+  await processMLData();
+  await processTaxonomyData();
+  await processEBirdData();
+  removeEmptyEntries();
+  replaceMlCatalogNumbers();
+  sortObservations();
+  writeJSONToFile(ebirdOutputFile, dataDict);
+  createSummaryData();
+};
+
+main();
