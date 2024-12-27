f82c534373696d8295655d4eb31432041adbb7b0
Rodrigo Dueñas <rduenasf@gmail.com>
2024-12-27 14:26:45 -0800

    update social card and favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,8 +81,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.png',
     docs: {
       sidebar: {
         hideable: true,
