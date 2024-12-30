7191b6bfcd75ed31dc19a712cfb5d4c713632e5e
Rodrigo Dueñas <rduenasf@gmail.com>
2024-12-29 17:24:30 -0800

    fix loading issue with fontawesome

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -4,6 +4,9 @@ import MDXComponents from "@theme-original/MDXComponents";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component.
 import { library } from "@fortawesome/fontawesome-svg-core"; // Import the library component.
 import { fas } from "@fortawesome/free-solid-svg-icons"; // Import all solid icons.
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 library.add(fas); // Add all icons to the library so you can use them without importing them individually.
 
